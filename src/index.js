07c607bad8f361fea5a1748d0acf39adfe819b05
zinzalasagar <sagarzinzala9@gmail.com>
2024-07-30 15:04:11 +0530

    project setup file

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// Import required packages
+const express = require("express");
+const bodyParser = require("body-parser");
+// require('dotenv').config();
+// Create an instance of the express app
+const app = express();
+
+// Set up middleware
+app.use(bodyParser.json());
+
+// Define routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Start the server
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
